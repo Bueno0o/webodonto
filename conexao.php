c00d4c3047a931a08dd30b089060a4b879a4db9d
Bueno0o <gabrielweb625@gmail.com>
2021-11-25 19:32:25 -0300

    webodonto 2

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
-    $host = "localhost";
-    $usuario = "root";
-    $senha = "";
-    $bd = "webodonto";
+    $host = "http://us-cdbr-east-04.cleardb.com/";
+    $usuario = "b31eeb187bf290";
+    $senha = "a66006e7";
+    $bd = "heroku_b19371aa9763c49";
 
     if(!$conexao = mysqli_connect($host, $usuario, $senha, $bd)){
         echo "Conexão com Banco de dados falhou";

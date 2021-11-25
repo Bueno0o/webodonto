981c446943b613cf33fa0664484e5daeacc758a1
Bueno0o <60942976+Bueno0o@users.noreply.github.com>
2021-11-25 19:38:13 -0300

    update

--- conexao.php
+++ conexao.php
@@ -1,5 +1,5 @@
 <?php
-    $host = "http://us-cdbr-east-04.cleardb.com/";
+    $host = "us-cdbr-east-04.cleardb.com";
     $usuario = "b31eeb187bf290";
     $senha = "a66006e7";
     $bd = "heroku_b19371aa9763c49";
